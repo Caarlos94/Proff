c7c9046438484aee64bafbfddd3c29bcdfd785bd
Caarlos94 <carlos94_cr7@hotmail.com>
2023-01-11 14:01:40 -0600

    deploy frontend

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ export const FILTRO = "FILTRO"
 
 export const filtro = ({ materia, dia, hora }) => {
     return function (dispatch) {
-        fetch(`http://localhost:3001/profe/?materia=${materia}&dia=${dia}&hora=${hora}`)
+        fetch(`https://proff-production.up.railway.app/profe/?materia=${materia}&dia=${dia}&hora=${hora}`)
         .then(response => response.json())
         .then((data) => dispatch({type: FILTRO, payload: data}))
     }
